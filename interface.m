5de515f10bb40018723ffa8bc49cd23b9e826115
L-I-P <l-i-p.fif@yandex.com>
2020-07-16 20:48:08 +0300

    Update interface.m

--- interface.m
+++ interface.m
@@ -75,7 +75,7 @@ varargout{1} = handles.output;
 
 % --- Executes on button press in pushbutton1.
 function pushbutton1_Callback(hObject, eventdata, handles)
-cla reset; %полный перебор и сброс осей
+cla reset; %РїРѕР»РЅС‹Р№ РїРµСЂРµР±РѕСЂ Рё СЃР±СЂРѕСЃ РѕСЃРµР№
 alpha = str2double(get(handles.lambda,'String'));
 beta = str2double(get(handles.beta,'String'));
 gamma = str2double(get(handles.gamma,'String'));
@@ -83,45 +83,45 @@ delta = str2double(get(handles.delta,'String'));
 lambda = str2double(get(handles.lambda,'String'));
 c = str2double(get(handles.c,'String')); %0.001 <= c <= 0.01
 rho = str2double(get(handles.rho,'String'));
-prey = 1;      %жертвы
-predators = 1; %хищники
+prey = 1;      %Р¶РµСЂС‚РІС‹
+predators = 1; %С…РёС‰РЅРёРєРё
 
-t0 = 0.1;                      %шаг
-seconds = 30;                  %время
-iterations = ceil(seconds/t0); %итерации
+t0 = 0.1;                      %С€Р°Рі
+seconds = 30;                  %РІСЂРµРјСЏ
+iterations = ceil(seconds/t0); %РёС‚РµСЂР°С†РёРё
 time = 0:t0:seconds;
 
-%инициализация массивов
+%РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РјР°СЃСЃРёРІРѕРІ
 x1 = zeros(1,iterations);
 x2 = zeros(1,iterations);
 psi = zeros(1,iterations);
 xi1 = zeros(1,iterations);
 
-% начальные значения (на 0 шаге. в матлабе с 1)
+% РЅР°С‡Р°Р»СЊРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ (РЅР° 0 С€Р°РіРµ. РІ РјР°С‚Р»Р°Р±Рµ СЃ 1)
 x1(1,1) = prey;
 x2(1,1) = predators;
-psi(1,1) = x1(1,1) - rho*x2(1,1); %по заданию разница стремится к нулю
-xi1(1,1) = normrnd(0,0.1); %шум только по координате с управлениям (м.о. равно 0, с.к.о. равно 0.1)
+psi(1,1) = x1(1,1) - rho*x2(1,1); %РїРѕ Р·Р°РґР°РЅРёСЋ СЂР°Р·РЅРёС†Р° СЃС‚СЂРµРјРёС‚СЃСЏ Рє РЅСѓР»СЋ
+xi1(1,1) = normrnd(0,0.1); %С€СѓРј С‚РѕР»СЊРєРѕ РїРѕ РєРѕРѕСЂРґРёРЅР°С‚Рµ СЃ СѓРїСЂР°РІР»РµРЅРёСЏРј (Рј.Рѕ. СЂР°РІРЅРѕ 0, СЃ.Рє.Рѕ. СЂР°РІРЅРѕ 0.1)
 
-%вычисление значений на k = 1 шаге для некоторых формул. это
-%связано с использованием значений k - 1 (например, x1)
+%РІС‹С‡РёСЃР»РµРЅРёРµ Р·РЅР°С‡РµРЅРёР№ РЅР° k = 1 С€Р°РіРµ РґР»СЏ РЅРµРєРѕС‚РѕСЂС‹С… С„РѕСЂРјСѓР». СЌС‚Рѕ
+%СЃРІСЏР·Р°РЅРѕ СЃ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµРј Р·РЅР°С‡РµРЅРёР№ k - 1 (РЅР°РїСЂРёРјРµСЂ, x1)
 xi1(1,2) = normrnd(0,0.1);
 x1(1,2) = -(lambda + c)*psi(1,1) + rho*(x2(1,1) + t0*(x2(1,1)*(delta*x1(1,1) - gamma))) + t0*(xi1(1,2) + c*xi1(1,1));
 x2(1,2) = x2(1,1) + t0*(x2(1,1)*(delta*x1(1,1) - gamma));
 
 for k = 2:iterations
-    xi1(1,k + 1) = normrnd(0,0.1); %шум только по координате с управлениям (м.о. равно 0, с.к.о. равно 0.1)
-    psi(1,k) = x1(1,k) - rho*x2(1,k); %по заданию разница стремится к нулю
+    xi1(1,k + 1) = normrnd(0,0.1); %С€СѓРј С‚РѕР»СЊРєРѕ РїРѕ РєРѕРѕСЂРґРёРЅР°С‚Рµ СЃ СѓРїСЂР°РІР»РµРЅРёСЏРј (Рј.Рѕ. СЂР°РІРЅРѕ 0, СЃ.Рє.Рѕ. СЂР°РІРЅРѕ 0.1)
+    psi(1,k) = x1(1,k) - rho*x2(1,k); %РїРѕ Р·Р°РґР°РЅРёСЋ СЂР°Р·РЅРёС†Р° СЃС‚СЂРµРјРёС‚СЃСЏ Рє РЅСѓР»СЋ
     x1(1,k + 1) = -(lambda + c)*psi(1,k) - c*lambda*psi(1,k - 1) + rho*(x2(1,k) + t0*(x2(1,k)*(delta*x1(1,k) - gamma))) + ...
         t0*(xi1(1,k + 1) + c*xi1(1,k));
     x2(1,k + 1) = x2(1,k) + t0*(x2(1,k)*(delta*x1(1,k) - gamma));
 end
 
-%отрисовка
+%РѕС‚СЂРёСЃРѕРІРєР°
 plot(time, x1, 'r', time, x2, 'b')
-animal = legend('жертвы','хищники');
-xlabel('время')
-ylabel('количество особей')
+animal = legend('Р¶РµСЂС‚РІС‹','С…РёС‰РЅРёРєРё');
+xlabel('РІСЂРµРјСЏ')
+ylabel('РєРѕР»РёС‡РµСЃС‚РІРѕ РѕСЃРѕР±РµР№')
 set(animal)
 line2 = sprintf('alpha = %.1f, beta=%.1f, gamma=%.1f, delta=%.1f, lambda=%.1f, c=%.3f, rho=%.1f, t0 = %.1f',...
     alpha, beta, gamma, delta, lambda, c, rho, t0);
